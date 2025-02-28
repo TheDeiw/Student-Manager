f6fe290422cf9602a7bb9b6c3fc45d5da2590950
TheDeiw <chehovskyi150@gmail.com>
2025-02-28 20:29:38 +0200

    Add-adding-student

--- js/side_menu.js
+++ js/side_menu.js
@@ -1,6 +1,8 @@
 const toggleButton = document.querySelector('.menu__icon_arrow')
 const sidebar = document.querySelector('.menu__container')
 
+document.querySelector('.menu__item.arrow').addEventListener('click', toggleSidebar)
+
 function toggleSidebar(){
   sidebar.classList.toggle('close')
   toggleButton.classList.toggle('close')
---
+++ js/table_control.js
@@ -0,0 +1,64 @@
+document.querySelector('.table__add_student').addEventListener('click', function(){
+	let tableBody = document.querySelector('.main_table tbody');
+
+	let newRow = document.createElement('tr');
+
+	function createCell(content){
+		let td = document.createElement("td");
+        if (typeof content === "string") {
+            td.textContent = content;
+        } else {
+            td.appendChild(content);
+        }
+        return td;
+	}
+
+	// 1️⃣ Checkbox
+	let checkbox = document.createElement("input");
+	checkbox.type = "checkbox";
+	newRow.appendChild(createCell(checkbox));
+
+	// 2️⃣ Group
+	newRow.appendChild(createCell("Group 2"));
+
+	// 3️⃣ Name
+	newRow.appendChild(createCell("Newsaaaaaaaaaaaaaaaaa User"));
+
+	// 4️⃣ Gender
+	newRow.appendChild(createCell("M"));
+
+	// 5️⃣ Birthday
+	newRow.appendChild(createCell("15.05.1995"));
+
+	// 6️⃣ Status (Активний круг)
+	let statusSpan = document.createElement("span");
+	statusSpan.classList.add("table__active_cirtle");
+	newRow.appendChild(createCell(statusSpan));
+
+	// 7️⃣ Option (Кнопки Редагувати / Видалити)
+	let optionsDiv = document.createElement("div");
+	optionsDiv.classList.add("table__cell_control");
+
+	let editButton = document.createElement("button");
+	editButton.classList.add("table__edit");
+	let editButtonIcon = document.createElement("img");
+	editButtonIcon.src = "img/table/edit.svg";
+	editButtonIcon.classList.add("table__icon");
+	editButton.appendChild(editButtonIcon);
+
+	let deleteButton = document.createElement("button");
+	deleteButton.classList.add("table__delete");
+	let deleteButtonIcon = document.createElement("img");
+	deleteButtonIcon.src = "img/table/delete.svg";
+	deleteButtonIcon.classList.add("table__icon");
+	deleteButton.appendChild(deleteButtonIcon);
+
+	optionsDiv.appendChild(editButton);
+	optionsDiv.appendChild(deleteButton);
+	newRow.appendChild(createCell(optionsDiv));
+
+
+	// Додаємо рядок у таблицю
+	tableBody.appendChild(newRow);
+
+})
