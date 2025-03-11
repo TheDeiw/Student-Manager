6a846221c4899daffe1daa0ebbd6447876b050b1
Dmytro Chekhovskyi <64646103+TheDeiw@users.noreply.github.com>
2025-03-12 01:25:05 +0200

    First try to make adaptability

--- js/side_menu.js
+++ js/side_menu.js
@@ -1,5 +1,6 @@
 const toggleButton = document.querySelector('.menu__icon_arrow')
 const sidebar = document.querySelector('.menu__container')
+const icon = document.querySelector('.nav_icon')
 
 document.querySelector('.menu__item.arrow').addEventListener('click', toggleSidebar)
 
@@ -7,3 +8,7 @@ function toggleSidebar(){
   sidebar.classList.toggle('close')
   toggleButton.classList.toggle('close')
 }
+
+icon.addEventListener('click', (event) => {
+  icon.classList.toggle("open");
+});
